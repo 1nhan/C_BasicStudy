a3aaa7cc8dd061907329ffe96ee0e60dd2801b80
1nhan <auroram4nia@gmail.com>
2021-06-16 17:42:25 +0900

    gg

---
+++ baekjoon_단계별/3for문/for_2438.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+	//	첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+	//	첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+	//	첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
+
+	int n;
+	scanf_s("%d", &n);
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
